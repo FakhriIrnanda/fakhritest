dd14fd72494042ab08dbc7349159d95b2a1d01de
FakhriIrnanda <fakhri.irnanda11@gmail.com>
2023-09-12 10:13:14 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<!-- Menampilkan array dengan menunjuk nomor indeks-nya -->
+
+<?php 
+$anak[0] = "fakhri"; 
+$anak[1] = "Muhammad Azzam Abqory"; 
+$anak[2] = "Raihan"; 
+echo "Isi array \$anak[0] adalah $anak[0] "; echo "<br>"; 
+echo "Isi array \$anak[1] adalah $anak[1] "; echo "<br>"; 
+echo "Isi array \$anak[2] adalah $anak[2]"; echo "<br>"; 
+?> 
